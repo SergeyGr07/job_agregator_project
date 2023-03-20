77a96bd4fac2c23f37aa2360f6dba2364569cac8
SergeyGr07 <49278850+SergeyGr07@users.noreply.github.com>
2023-03-20 16:59:11 +0800

    worked_feature

--- parser/chrome_driver/selenium_python.py
+++ parser/chrome_driver/selenium_python.py
@@ -1,23 +1,35 @@
 import time
 from selenium import webdriver
 
-url = "https://kino-mall.ru/schedule/?date=20.03.2023"
+# Устанавливаем ссылки на страницы, которые нужно проверять
+url = "https://kino-mall.ru/schedule/?date=23.03.2023"
+
+# Инициализируем драйвер браузера
 driver = webdriver.Chrome(executable_path="D:\job_agregator_project\parser\chrome_driver\chromedriver.exe")
 
 try:
+    # Открываем страницу
+    print("Opening page...")
+    driver.get(url)
 
-    print("Processing...")
     while True:
-        driver.get(url)
+        # Обновляем страницу
+        print("Checking page...")
         driver.refresh()
+        print("Processing...")
+        # Ждем 1(3600) час
         time.sleep(10)
-        if driver.current_url != url:
+        print("Checking the page...Please wait...")
+        # Проверяем, изменилась ли ссылка на страницу
+        if driver.current_url == url:
             print("Ссылка изменилась!")
             break
-        print("Processing...")
+
 except Exception as e:
+    # Обрабатываем ошибки
     print("Произошла ошибка: ", e)
 
 finally:
+    # Закрываем браузер
     driver.close()
     driver.quit()
